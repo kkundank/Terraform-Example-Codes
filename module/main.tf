b40511c35b26eb88ffdb956a0656eec80045adf0
Kundan Kumar <kumar.kundan718@gmail.com>
2019-08-06 12:14:12 +0000

    Changes to main.tf

--- module/main.tf
+++ module/main.tf
@@ -2,7 +2,13 @@
 # Project: CNHI 
 # Calling Required Modules
 #############################################
-
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "us-west-2"
+}
 resource "aws_sns_topic" "sns_notification" {
   name = "iTrams-CNHI-PROD-SNS01"
 }
